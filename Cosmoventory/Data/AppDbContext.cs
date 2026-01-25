0dca53375861252a5c5bf5c164ef9efaa57aae63
Mahan <megamindmaahan@gmail.com>
2026-01-25 14:55:45 -0500

    Created ApplicationDbContext

---
+++ Cosmoventory/Data/AppDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Cosmoventory.Data;
+
+public class AppDbContext: DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+    {
+
+    }
+}
