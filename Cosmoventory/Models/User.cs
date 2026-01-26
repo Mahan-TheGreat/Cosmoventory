53a34e22a181ccee685321726f84ef2cba07232e
Mahan <megamindmaahan@gmail.com>
2026-01-26 14:07:48 -0500

    Created User Model

---
+++ Cosmoventory/Models/User.cs
@@ -0,0 +1,34 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Cosmoventory.Models
+{
+    public class User
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required, MaxLength(50)]
+        public string Username { get; set; } = null!;
+
+        [Required, MaxLength(100)]
+        public string Email { get; set; } = null!;
+
+        [Required]
+        public string PasswordHash { get; set; } = null!;
+
+        // Role & Status
+        [Required, MaxLength(20)]
+        public string Role { get; set; } = "Staff";
+
+
+        // Auditing
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+        public DateTime? UpdatedAt { get; set; }
+        public DateTime? LastLoginAt { get; set; }
+
+        public bool IsLocked { get; set; } = true;
+
+        // Soft delete 
+        public bool IsActive { get; set; } = true;
+    }
+}
