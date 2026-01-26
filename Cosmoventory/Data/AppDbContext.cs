4874404c499a089b286fe617d13039b9eeee3b93
Mahan <megamindmaahan@gmail.com>
2026-01-26 14:07:35 -0500

    Added ApplicationDbContext

--- Cosmoventory/Data/AppDbContext.cs
+++ Cosmoventory/Data/AppDbContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Cosmoventory.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace Cosmoventory.Data;
 
@@ -8,4 +9,6 @@ public class AppDbContext: DbContext
     {
 
     }
+
+    public DbSet<User> Users { get; set; }
 }
