7671a3f57d03e70f3ce403830e7a895ccce2910c
Mahan <megamindmaahan@gmail.com>
2026-01-27 10:54:50 -0500

    Added JWT Configuration

---
+++ Cosmoventory/Controllers/AuthController.cs
@@ -0,0 +1,91 @@
+﻿using Cosmoventory.Data;
+using Cosmoventory.DTO;
+using Cosmoventory.Enums;
+using Cosmoventory.Helpers;
+using Cosmoventory.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Data;
+
+namespace Cosmoventory.Controllers
+{
+    [Route("api/Auth")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly AppDbContext _db;
+        private readonly JwtTokenService _jwt;
+
+        public AuthController(AppDbContext db, JwtTokenService jwt)
+        {
+            _db = db;
+            _jwt = jwt;
+        }
+        [HttpPost("register")]
+        public async Task<IActionResult> Register(RegisterUserRequestDTO request)
+        {
+            // Check username/email uniqueness
+            var exists = await _db.Users.AnyAsync(u =>
+                u.Username == request.Username || u.Email == request.Email);
+
+            if (exists)
+                return BadRequest("Username or email already exists.");
+
+            var user = new User
+            {
+                Username = request.Username,
+                Email = request.Email,
+                PasswordHash = PasswordHashHelper.Hash(request.Password),
+                Role = OrganizationRole.staff.ToString(),        // Default role
+                IsActive = true
+            };
+
+            _db.Users.Add(user);
+            await _db.SaveChangesAsync();
+
+            return CreatedAtAction(nameof(Register), new
+            {
+                user.Id,
+                user.Username,
+                user.Email,
+                user.Role
+            });
+        }
+
+        [HttpPost("login")]
+        public async Task<IActionResult> Login(LoginRequestDTO request)
+        {
+            var user = await _db.Users.SingleOrDefaultAsync(u =>
+                u.Username == request.Username);
+
+            if (user == null)
+                return Unauthorized("Invalid credentials.");
+
+            if (!user.IsActive || user.IsLocked)
+                return Unauthorized("Account is disabled.");
+
+            if (!PasswordHashHelper.Verify(user.PasswordHash, request.Password))
+                return Unauthorized("Invalid credentials.");
+
+            user.LastLoginAt = DateTime.UtcNow;
+            await _db.SaveChangesAsync();
+
+            var token = _jwt.GenerateToken(user.Id, user.Username, user.Role);
+
+            return Ok(new
+            {
+                token,
+                user = new
+                {
+                    user.Id,
+                    user.Username,
+                    user.Email,
+                    user.Role
+                }
+            });
+        }
+    }
+
+}
--- Cosmoventory/Program.cs
+++ Cosmoventory/Program.cs
@@ -1,5 +1,8 @@
 using Cosmoventory.Data;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -12,6 +15,34 @@ builder.Services.AddOpenApi();
 builder.Services.AddDbContext<AppDbContext>(options =>
 options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")
 ));
+
+
+// -- For Authentication
+var jwtSettings = builder.Configuration.GetSection("Jwt");
+var key = Encoding.UTF8.GetBytes(jwtSettings["Key"]!);
+
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+})
+    .AddJwtBearer(options =>
+    {
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuer = true,
+            ValidateAudience = true,
+            ValidateLifetime = true,
+            ValidateIssuerSigningKey = true,
+            ValidIssuer = jwtSettings["Issuer"],
+            ValidAudience = jwtSettings["Audience"],
+            IssuerSigningKey = new SymmetricSecurityKey(key)
+        };
+    });
+builder.Services.AddAuthorization();
+builder.Services.AddScoped<JwtTokenService>();
+// -- Authentication Complete
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
