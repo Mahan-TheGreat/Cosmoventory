b35455dffffd3329e0c4db075a2d805f578563b0
Mahan <megamindmaahan@gmail.com>
2026-01-27 10:59:07 -0500

    Created DTO for Login Request and RegisterUserRequest

---
+++ Cosmoventory/DTO/LoginRequestDTO.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Cosmoventory.DTO
+{
+    public class LoginRequestDTO
+    {
+        [Required]
+        public string Username { get; set; } = null!;
+
+        [Required]
+        public string Password { get; set; } = null!;
+    }
+}
---
+++ Cosmoventory/DTO/RegisterUserRequestDTO.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Cosmoventory.DTO
+{
+    public class RegisterUserRequestDTO
+    {
+        [Required, MaxLength(50)]
+        public string Username { get; set; } = null!;
+
+        [Required, EmailAddress, MaxLength(100)]
+        public string Email { get; set; } = null!;
+
+        [Required, MinLength(6)]
+        public string Password { get; set; } = null!;
+    }
+}
