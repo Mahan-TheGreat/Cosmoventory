4cda6643ef99da3eb06daaaf69cd790f92fdfaea
Mahan <megamindmaahan@gmail.com>
2026-01-27 10:55:33 -0500

    Helper to hash the password

---
+++ Cosmoventory/Helpers/PasswordHashHelper.cs
@@ -0,0 +1,15 @@
+﻿using Cosmoventory.Models;
+using Microsoft.AspNetCore.Identity;
+
+namespace Cosmoventory.Helpers;
+public class PasswordHashHelper
+{
+    private static readonly PasswordHasher<User> Hasher = new();
+
+    public static string Hash(string plainPassword)
+        => Hasher.HashPassword(null!, plainPassword);
+
+    public static bool Verify(string hashedPassword, string plainPassword)
+        => Hasher.VerifyHashedPassword(null!, hashedPassword, plainPassword)
+        == PasswordVerificationResult.Success;
+}
