3184fc1018d29118cc823c2db3f861f9cd67b1ed
Mahan <megamindmaahan@gmail.com>
2026-01-30 12:59:23 -0500

    Updated the Program.js to use Swagger Api

--- Cosmoventory/Program.cs
+++ Cosmoventory/Program.cs
@@ -1,7 +1,9 @@
-using Cosmoventory.Data;
+﻿using Cosmoventory.Data;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi;
+
 using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -43,12 +45,53 @@ builder.Services.AddAuthorization();
 builder.Services.AddScoped<JwtTokenService>();
 // -- Authentication Complete
 
+//-- Adding Swagger
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(options =>
+{
+    options.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Title = "Cosmoventory API",
+        Version = "v1"
+    });
+    //// 🔐 JWT Authorization support
+    //options.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+    //{
+    //    Name = "Authorization",
+    //    Type = SecuritySchemeType.Http,
+    //    Scheme = "Bearer",
+    //    BearerFormat = "JWT",
+    //    In = ParameterLocation.Header,
+    //    Description = "Enter: Bearer {your JWT token}"
+    //});
+
+    //options.AddSecurityRequirement(new OpenApiSecurityRequirement
+    //{
+    //    {
+    //        new OpenApiSecurityScheme
+    //        {
+                
+    //            Type:"",
+    //            Scheme:""
+    //        },
+    //        Array.Empty<string>()
+    //    }
+    //});
+});
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+        c.RoutePrefix = ""; // Loads Swagger at root URL
+    });
 }
 
 app.UseHttpsRedirection();
