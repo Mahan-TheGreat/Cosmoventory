49a171915ab09ff550a476c78104f4ae8d7bba09
Mahan <megamindmaahan@gmail.com>
2026-02-11 23:29:20 -0500

    Added RefreshToken Model

---
+++ Cosmoventory/Models/RefreshToken.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Cosmoventory.Models
+{
+    public class RefreshToken
+    {
+        public int Id { get; set; }
+        public string Token { get; set; }
+        public int UserId { get; set; }
+        public DateTime ExpiresAt { get; set; }
+        public bool IsRevoked { get; set; } = false;
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+
+        [ForeignKey("UserId")]
+        public virtual User User { get; set; }
+    }
+}
