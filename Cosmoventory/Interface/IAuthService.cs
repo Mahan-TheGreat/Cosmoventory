95580c29a68d6b07ee41f700f51bc8949ea53639
Mahan <megamindmaahan@gmail.com>
2026-02-14 19:35:59 -0500

    Added; Interface for Authentication Services

---
+++ Cosmoventory/Interface/IAuthService.cs
@@ -0,0 +1,10 @@
+﻿namespace Cosmoventory.Interface
+{
+    public interface IAuthService
+    {
+        Task<AuthResponseDTO?> LoginAsync(string username, string password, string ipAddress);
+        Task<AuthResponseDTO?> RefreshAsync(string refreshToken, string ipAddress);
+        Task<bool> LogoutAsync(string refreshToken, string ipAddress);
+        Task RevokeAllAsync(int userId, string ipAddress);
+    }
+}
