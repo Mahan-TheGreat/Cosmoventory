271a00766485a76d1fab2fb8b57d06df4dd4fb44
Mahan <megamindmaahan@gmail.com>
2026-02-14 19:37:29 -0500

    Update: Modified Refresh Token. Added variable IsExpired and IsActive

--- Cosmoventory/Models/RefreshToken.cs
+++ Cosmoventory/Models/RefreshToken.cs
@@ -8,10 +8,13 @@ namespace Cosmoventory.Models
         public string Token { get; set; }
         public int UserId { get; set; }
         public DateTime ExpiresAt { get; set; }
-        public bool IsRevoked { get; set; } = false;
+        public DateTime? RevokedAt { get; set; }
         public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+        public bool IsExpired => DateTime.UtcNow >= ExpiresAt;
+        public bool IsActive => RevokedAt == null && !IsExpired;
 
         [ForeignKey("UserId")]
         public virtual User User { get; set; }
+       
     }
 }
